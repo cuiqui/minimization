515f65ae94b3ed9a89e887813068d56afd54a9f4
Juan Schandin <jschandin@gmail.com>
2025-06-23 16:42:46 -0300

    lint

--- tests/test_minimize.py
+++ tests/test_minimize.py
@@ -31,8 +31,7 @@ def test_minimize_figure_4_8():
     )
     assert equivalent(dfa, minimized_dfa)
     assert minimized_dfa.start_state == frozenset({'A', 'E'})
-    assert minimized_dfa.states == {AE, C, G, BH, DF
-    }
+    assert minimized_dfa.states == {AE, C, G, BH, DF}
     assert minimized_dfa.transition == {
         (AE, '0'): BH,
         (AE, '1'): DF,
